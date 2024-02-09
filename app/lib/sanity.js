0673221ef03b12fa3f8c7f1972fc8ea011c4a51e
Braxton <58635532+Braxton-Jones@users.noreply.github.com>
2024-02-09 12:23:04 -0500

    api fix

--- app/lib/sanity.js
+++ app/lib/sanity.js
@@ -1,14 +1,9 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
-const getTodaysApiVersion = () => {
-    const today = new Date();
-    return `${today.getFullYear()}-${String(today.getMonth() +  1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-  };
-
 const projectId = 'jio333cn';
 const dataset = 'production';
-const apiVersion = getTodaysApiVersion();
+const apiVersion = `2021-10-18`;
 
 
  export const sanityxClient = createClient({
